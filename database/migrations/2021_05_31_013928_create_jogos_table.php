de87681e7a144c44e6aabc4a6884f4d345ddc300
victorMendes18 <victorfran18@gmail.com>
2021-05-30 22:46:53 -0300

    criando classe Jogo e sua migration

---
+++ database/migrations/2021_05_31_013928_create_jogos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJogosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogos', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jogos');
+    }
+}
