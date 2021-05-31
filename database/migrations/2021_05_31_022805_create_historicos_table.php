ea7af5f36b12ffa6d178b8eb23748384782c50cc
victorMendes18 <victorfran18@gmail.com>
2021-05-30 23:33:50 -0300

    Criando classe Historico e sua migration

---
+++ database/migrations/2021_05_31_022805_create_historicos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoricosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historicos', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->text('nome_do_jogador');
+            $table->Integer('jogadas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historicos');
+    }
+}
