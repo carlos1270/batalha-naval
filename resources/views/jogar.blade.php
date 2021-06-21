0973be83a7f35b3f54c077691bbc6a0be188ef00
KelwinJonas <kelwinjonas@gmail.com>
2021-06-21 00:04:27 -0300

    Adicionando tabuleiro COM a view de jogar

--- resources/views/jogar.blade.php
+++ resources/views/jogar.blade.php
@@ -10,20 +10,40 @@
         @endforeach
     </div>
 
+    <div id="casasTabuleiroPlayer">
+        @foreach ($jogo->tabuleiros[0]->casas as $casa)
+            <input type="hidden" id="casaPlayer{{$casa->linha}}x{{$casa->coluna}}" value="{{$casa->id}}">
+        @endforeach
+    </div>
+
     <div id="naviosTabuleiroCOM">
         @foreach ($jogo->tabuleiros[1]->navios as $i => $navio)
-            <input type="hidden" id="navio{{$i+1}}" value="{{$navio->id}}">
-            <input type="hidden" id="tamanho_navio{{$i+1}}" value="{{$navio->tamanho}}">
+            <input type="hidden" id="navioCOM{{$i+1}}" value="{{$navio->id}}">
+            <input type="hidden" id="tamanho_navioCOM{{$i+1}}" value="{{$navio->tamanho}}">
+        @endforeach
+    </div>
+
+    <div id="naviosTabuleiroPlayer">
+        @foreach ($jogo->tabuleiros[0]->navios as $i => $navio)
+            <input type="hidden" id="navioPlayer{{$i+1}}" value="{{$navio->id}}">
+            <input type="hidden" id="tamanho_navioPlayer{{$i+1}}" value="{{$navio->tamanho}}">
         @endforeach
     </div>
 
     <script>
-        var telaLargura = 930;
+        var telaLargura = 1280;
         var telaAltura = 615;
-        var navios = {//aqui defini onde os navios vao spawnar na tela e a posicao
+        var naviosCOM = {//aqui defini onde os navios vao spawnar na tela e a posicao
         };
-        var casas = {//object pra guardar as casas
+        var naviosPlayer = {
         };
+        var casasCOM = {//object pra guardar as casas
+        };
+        var casasPlayer = {
+        };
+        var vezDoJogador = true;
+        var casasDisponiveis = []; //para o COM jogar
+
         const tamanhoTabuleiro = 10;
         const espacoEntreCasas = 55; //relativo ao tamanho em pixels da celula
         const espacoDeEncaixe = 28; //pra encaixar o navio na celula
@@ -37,6 +57,7 @@
         });
 
         var navioLayer = new Konva.Layer();
+        var casaLayer = new Konva.Layer();
         var images = {};
 
         var naviosSources = {//source de onde fica os navios
@@ -77,48 +98,33 @@
             }
         };
 
-        function loadNavioAfundado(sourceNavio, navio, cas, nav){
+        function loadNavioAfundado(sourceNavio, navio, cas, nav, casas, navios){
             images[sourceNavio] = new Image();
             images[sourceNavio].onload = function(){
                 navio.image(images[sourceNavio]);
                 navio.setX(nav.x);
                 navio.setY(nav.y);
-                navio.rotation(getRotacaoNavio(cas));
+                navio.rotation(getRotacaoNavio(cas, casas, navios));
             }
             stage.add(navioLayer);
             images[sourceNavio].src = naviosSources[sourceNavio];
-        }
+        };
 
         function initNaviosCasas(){
             for(let i = 1; i <= tamanhoTabuleiro; i++){ //criacao das casas
                 for(let j = 1; j <= tamanhoTabuleiro; j++){
-                    casas['casa'+j+'x'+i] = {x: espacoEntreCasas*i, y: espacoEntreCasas*j, linha: j, coluna: i, ocupada: "0", navio: "", id: document.getElementById('casaCOM'+j+'x'+i).value, posicao: "-1", acertado: false,}; //cria as casas dando espaco e nome unico
+                    casasCOM['casa'+j+'x'+i] = {x: espacoEntreCasas*i, y: espacoEntreCasas*j, linha: j, coluna: i, ocupada: "0", navio: "", id: document.getElementById('casaCOM'+j+'x'+i).value, posicao: "-1", acertado: false,}; //cria as casas dando espaco e nome unico
+                    casasPlayer['casa'+j+'x'+i] = {x: espacoEntreCasas*i+(espacoEntreCasas*(tamanhoTabuleiro+2)), y: espacoEntreCasas*j, linha: j, coluna: i, ocupada: "0", navio: "", id: document.getElementById('casaPlayer'+j+'x'+i).value, posicao: "-1", acertado: false,};
+                    casasDisponiveis.push(document.getElementById('casaPlayer'+j+'x'+i).value);
                 }
             };
             for(let i = 1; i <= quantidadeNavios; i++){
-                navios['navio'+i] = {x: espacoEntreCasas*(tamanhoTabuleiro+1), y: (espacoEntreCasas*(i+1)), vida: parseInt(document.getElementById('tamanho_navio'+i).value), tamanho: parseInt(document.getElementById('tamanho_navio'+i).value), id: document.getElementById('navio'+i).value};
+                naviosCOM['navio'+i] = {x: espacoEntreCasas*(tamanhoTabuleiro+1), y: (espacoEntreCasas*(i+1)), vida: parseInt(document.getElementById('tamanho_navioCOM'+i).value), tamanho: parseInt(document.getElementById('tamanho_navioCOM'+i).value), id: document.getElementById('navioCOM'+i).value};
+                naviosPlayer['navioP'+i] = {x: espacoEntreCasas*(tamanhoTabuleiro+1), y: (espacoEntreCasas*(i+1)), vida: parseInt(document.getElementById('tamanho_navioPlayer'+i).value), tamanho: parseInt(document.getElementById('tamanho_navioPlayer'+i).value), id: document.getElementById('navioPlayer'+i).value};
             };
         };
 
-        function getCasa(nav){
-            for(let key in casas){
-                if(casas[key].ocupada == "1"){
-                    if(nav.id == casas[key].navio){
-                        let cas = casas[key];
-                        if(cas.posicao == 1 && getRotacaoNavio(cas) == 0){
-                            return cas;
-                        }else{
-                            if(cas.posicao == nav.tamanho && getRotacaoNavio(cas) == 270){
-                                return cas;
-                            }
-                        }
-                    }
-                }
-            }
-        };
-
-
-        function getNavio(cas){
+        function getNavio(cas, navios){
             for(let key in navios){
                 if(cas.navio == navios[key].id){
                     return navios[key];
@@ -126,24 +132,45 @@
             }
         };
 
-        function getNavioTamanho(cas){
-            let nav = getNavio(cas);
+        function getNavioTamanho(cas, navios){
+            let nav = getNavio(cas, navios);
             return nav.tamanho;
         };
 
-        function getNavioKonva(nav){
+        function getNavioKonva(nav, navios){
             let imagesNavios = stage.find('.navio');
             for(let key in imagesNavios){
                 navio = imagesNavios[key];
                 let navtemp = navios[navio.id()];
-                if(navtemp.id == nav.id){
-                    return navio;
+                if(navtemp != undefined){
+                    if(navtemp.id == nav.id){
+                        return navio;
+                    }
                 }
             }
         };
 
-        function getRotacaoNavio(cas){
-            let nav = getNavio(cas);
+        function getCasaConva(cas, casas){
+            let imagesCasa = stage.find('.casa');
+            for(let key in imagesCasa){
+                casa =  imagesCasa[key];
+                let castemp = casas[casa.id()];
+                if(castemp.id == cas.id){
+                    return casa;
+                }
+            }
+        };
+
+        function getCasaPorID(id, casas){
+            for(let key in casas){
+                if(casas[key].id == id){
+                    return casas[key];
+                }
+            }
+        };
+
+        function getRotacaoNavio(cas, casas, navios){
+            let nav = getNavio(cas, navios);
             for(let key in casas){
                 let casatemp = casas[key];
                 if(casatemp.navio == cas.navio){
@@ -160,7 +187,7 @@
             }
         };
 
-        function getCasaPlot(cas, nav, angulo){
+        function getCasaPlot(cas, nav, angulo, casas){
             if(angulo == 0){
                 for(let key in casas){
                     let casatemp = casas[key];
@@ -178,32 +205,32 @@
             }
         };
 
-        function setNaviosPosicoes(cas){
-                let nav = getNavio(cas);
-                let angulo = getRotacaoNavio(cas);
-                let casatemp = getCasaPlot(cas, nav, angulo);
-                if(angulo == 0){
-                    nav.x = casatemp.x+5;
-                    nav.y = casatemp.y+5;
-                }else{
-                    nav.x = casatemp.x+5;
-                    nav.y = casatemp.y+espacoDeEncaixe+15;
-                }
-        };
-
-        function navioAtingido(cas){
-            let nav = getNavio(cas);
-            nav.vida -= 1;
-        };
-
-        function afundarNavio(cas){
-            let nav = getNavio(cas);
-            if(nav.vida == 0){
-                loadNavioAfundado(getNavioSource(nav), getNavioKonva(nav), cas, nav);
+        function setNaviosPosicoes(cas, navios, casas){
+            let nav = getNavio(cas, navios);
+            let angulo = getRotacaoNavio(cas, casas, navios);
+            let casatemp = getCasaPlot(cas, nav, angulo, casas);
+            if(angulo == 0){
+                nav.x = casatemp.x+5;
+                nav.y = casatemp.y+5;
+            }else{
+                nav.x = casatemp.x+5;
+                nav.y = casatemp.y+espacoDeEncaixe+15;
             }
         };
 
-        function initStageNavios() {
+        function navioAtingido(cas, navios){
+            let nav = getNavio(cas, navios);
+            nav.vida -= 1;
+        };
+
+        function afundarNavio(cas, navios, casas){
+            let nav = getNavio(cas, navios);
+            if(nav.vida == 0){
+                loadNavioAfundado(getNavioSource(nav), getNavioKonva(nav, navios), cas, nav, casas, navios);
+            }
+        };
+
+        function initStageNavios(navios) {
             for (let key in navios) {
                 (function () {
                     let nav = navios[key];
@@ -219,12 +246,11 @@
             stage.add(navioLayer);
         };
 
-        function initStageCasas(images){
-            var casaLayer = new Konva.Layer();
-            for (let key in casas) {
+        function initStageCasasCOM(images){
+            for (let key in casasCOM) {
                 (function () {
                     let imageObj = images[key];
-                    let cas = casas[key];
+                    let cas = casasCOM[key];
 
                     let casa = new Konva.Image({
                         x: cas.x,
@@ -247,48 +273,51 @@
                     });
 
                     casa.on('click', function() {
-                        if(!cas.acertado){
-                            cas.acertado = true;
-                            $.ajax({
-                                url: "{{route('atirar')}}",
-                                type: "GET",
-                                data: {
-                                    casa_id:    cas.id,
-                                },
-                                statusCode: {
-                                    310: function(data){
-                                        casa.image(images['cell_board_water']);
+                        if(vezDoJogador){
+                            if(!cas.acertado){
+                                cas.acertado = true;
+                                $.ajax({
+                                    url: "{{route('atirar')}}",
+                                    type: "GET",
+                                    data: {
+                                        casa_id:    cas.id,
                                     },
-                                    311: function(data){
-                                        cas.navio = data.responseJSON.navio_id;
-                                        cas.posicao = data.responseJSON.posicao_do_navio;
-                                        cas.ocupada = "1";
-                                        casa.image(images['cell_board_bomb']);
-
-                                        navioAtingido(cas);
+                                    statusCode: {
+                                        310: function(data){
+                                            casa.image(images['cell_board_water']);
+                                            vezDoJogador = false;
+                                            realizarJogadaCOM();
+                                        },
+                                        311: function(data){
+                                            cas.navio = data.responseJSON.navio_id;
+                                            cas.posicao = data.responseJSON.posicao_do_navio;
+                                            cas.ocupada = "1";
+                                            casa.image(images['cell_board_bomb']);
+                                            navioAtingido(cas, naviosCOM);
+                                        },
+                                        312: function(data){
+                                            cas.navio = data.responseJSON.navio_id;
+                                            cas.posicao = data.responseJSON.posicao_do_navio;
+                                            cas.ocupada = "1";
+                                            casa.image(images['cell_board_bomb']);
+                                            setNaviosPosicoes(cas, naviosCOM, casasCOM);
+                                            navioAtingido(cas, naviosCOM);
+                                            afundarNavio(cas, naviosCOM, casasCOM);
+                                        },
+                                        313: function(data){
+                                            cas.navio = data.responseJSON.navio_id;
+                                            cas.posicao = data.responseJSON.posicao_do_navio;
+                                            cas.ocupada = "1";
+                                            casa.image(images['cell_board_bomb']);
+                                            setNaviosPosicoes(cas, naviosCOM, casasCOM);
+                                            navioAtingido(cas, naviosCOM);
+                                            afundarNavio(cas, naviosCOM, casasCOM);
+                                        },
                                     },
-                                    312: function(data){
-                                        cas.navio = data.responseJSON.navio_id;
-                                        cas.posicao = data.responseJSON.posicao_do_navio;
-                                        cas.ocupada = "1";
-                                        casa.image(images['cell_board_bomb']);
-                                        setNaviosPosicoes(cas);
-                                        navioAtingido(cas);
-                                        afundarNavio(cas);
-                                    },
-                                    313: function(data){
-                                        cas.navio = data.responseJSON.navio_id;
-                                        cas.posicao = data.responseJSON.posicao_do_navio;
-                                        cas.ocupada = "1";
-                                        casa.image(images['cell_board_bomb']);
-                                        setNaviosPosicoes(cas);
-                                        navioAtingido(cas);
-                                        afundarNavio(cas);
-                                    },
-                                },
-                            })
-                        }else{
-                            alert("Casa já acertada");
+                                });
+                            }else{
+                                alert("Casa já acertada");
+                            }
                         }
                     });
 
@@ -299,6 +328,80 @@
         };
 
 
+        function realizarJogadaCOM(){
+            let IDCasa = selecionarCasaAleatoria();
+            let casaJogada = getCasaPorID(IDCasa, casasPlayer);
+            resultadoJogadaCOM(casaJogada, getCasaConva(casaJogada, casasPlayer));
+        };
+
+        function selecionarCasaAleatoria(){
+            let selecionado = casasDisponiveis[Math.floor(Math.random()*casasDisponiveis.length)];
+            casasDisponiveis.splice(casasDisponiveis.indexOf(selecionado), 1);
+            return selecionado;
+        };
+
+        function resultadoJogadaCOM(cas, casa){
+            cas.acertado = true;
+            $.ajax({
+                url: "{{route('atirar')}}",
+                type: "GET",
+                data: {
+                    casa_id:    cas.id,
+                },
+                statusCode: {
+                    310: function(data){
+                        casa.image(images['cell_board_water']);
+                        vezDoJogador = true;
+                    },
+                    311: function(data){
+                        cas.navio = data.responseJSON.navio_id;
+                        cas.posicao = data.responseJSON.posicao_do_navio;
+                        cas.ocupada = "1";
+                        casa.image(images['cell_board_bomb']);
+                        navioAtingido(cas, naviosPlayer);
+                        realizarJogadaCOM();
+                    },
+                    312: function(data){
+                        cas.navio = data.responseJSON.navio_id;
+                        cas.posicao = data.responseJSON.posicao_do_navio;
+                        cas.ocupada = "1";
+                        casa.image(images['cell_board_bomb']);
+                        setNaviosPosicoes(cas, naviosPlayer, casasPlayer);
+                        navioAtingido(cas, naviosPlayer);
+                        afundarNavio(cas, naviosPlayer, casasPlayer);
+                        realizarJogadaCOM();
+                    },
+                    313: function(data){
+                        cas.navio = data.responseJSON.navio_id;
+                        cas.posicao = data.responseJSON.posicao_do_navio;
+                        cas.ocupada = "1";
+                        casa.image(images['cell_board_bomb']);
+                        setNaviosPosicoes(cas, naviosPlayer, casasPlayer);
+                        navioAtingido(cas, naviosPlayer);
+                        afundarNavio(cas, naviosPlayer, casasPlayer);
+                    },
+                },
+            })
+        };
+
+        function initStageCasasPlayer(images){
+            for (let key in casasPlayer) {
+                (function () {
+                    let imageObj = images[key];
+                    let cas = casasPlayer[key];
+
+                    let casa = new Konva.Image({
+                        x: cas.x,
+                        y: cas.y,
+                        image: imageObj,
+                        name: 'casa',
+                        id: ''+key,
+                    });
+                    casaLayer.add(casa);
+                })();
+            }
+            stage.add(casaLayer);
+        };
 
       var casasSources = {
           cell_board_mouseover: '{{asset('img/cell_board_mouseover.png')}}',
@@ -313,8 +416,10 @@
         };
 
         initNaviosCasas();
-        loadImages(casasSources, initStageCasas);//carrega o stage pra iniciar os bagulhos
-        initStageNavios();
+        loadImages(casasSources, initStageCasasCOM);//carrega o stage pra iniciar os bagulhos
+        loadImages(casasSources, initStageCasasPlayer);//carrega o stage pra iniciar os bagulhos
+        initStageNavios(naviosCOM);
+        initStageNavios(naviosPlayer);
 
     </script>
 @endsection
