a5f24ef23aa6ff3a2eace0eee4578b2a998efca0
Jackson <jacksonlmp17.jl@gmail.com>
2021-06-22 21:39:56 -0300

    Editando tela de jogar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <script src="https://unpkg.com/konva@8.0.4/konva.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <head>
+        <script src="https://unpkg.com/konva@8.0.4/konva.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
 <body>
 
     @yield('content')
---
+++ resources/views/layouts/jogar-template.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <script src="https://unpkg.com/konva@8.0.4/konva.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <meta charset="utf-8" />
+        <title>Batalha Naval - JOGAR</title>
+        <link rel="stylesheet" href="{{asset('css/jogar.css')}}">
+    </head>
+    <body>
+        @yield('content')
+
+    </body>
+</html>
