165d47e2ec39a4069a3b5d4592f9dbeb23ea8d09
Carlos André <carlos.andre1270@gmail.com>
2021-07-03 13:32:06 -0300

    icon e title da aplicação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-
+        <title>Batalha Naval</title>
+        <link rel="icon" type="image/png" href="{{asset('img/icon-app.png')}}">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
 <body>
